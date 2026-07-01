c511778f44a64d874a360158288a112a4b953368
solar262 <solardynamics592@gmail.com>
2026-07-01 22:01:14 +0200

    abc

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
